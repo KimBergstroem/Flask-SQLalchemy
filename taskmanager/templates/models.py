e78eeb8e08d955291e857ea261604c663c3f3885
KimBergstroem <>
2023-08-09 11:30:51 +0000

    Update changes to database configuration

--- taskmanager/templates/models.py
+++ taskmanager/templates/models.py
@@ -1,10 +1,12 @@
-from taskmanager import db 
+from taskmanager import db
+
 
 class Category(db.Model):
     # schema for category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    
+    tasks = db.relationship("Task", backref="category",
+                            cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # repr to represent itself in the form of a string
@@ -14,8 +16,14 @@ class Category(db.Model):
 class Task(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    task_name = db.Column(db.string(50), unique=True, nullable=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, defualt=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False) # CASCADE IS FOR DELETIN ALL TASK IN THAT CATEGORY
+    # CASCADE IS FOR DELETIN ALL TASK IN THAT CATEGORY
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        "category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # repr to represent itself in th form of a string
+        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"
