82b69cc2e3b2a235895648e71243d4c29038bc2f
KimBergstroem <>
2023-08-09 14:49:35 +0000

    Installation of category in the hmtl page

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -9,11 +9,18 @@ def home():
 
 
 @app.route("/categories")
-def categories():
-    return render_template("categories.html")
+def categories():  # Will retreive all the data into the variable
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+    # first categories present the html page, second present the list above function
 
 
 # GET AND POST IS NEEDED TO HAVE THE STUFF INTO DATABASE
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
