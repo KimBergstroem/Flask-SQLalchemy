b28981c7936ad5d4623555b2ed49150976345569
KimBergstroem <>
2023-08-09 12:24:36 +0000

    Initial base template html structure to frontend the page

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+/* JAVASCRIPT SCRIPT FOR MY TASKMANAGER PROJECT */
+
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav, m.Sidenav);
+});
