e2b2afc74b0c30b6989a358f4350f57f09401ea4
KimBergstroem <>
2023-08-09 16:17:57 +0000

    Add: delete func, remove category

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -24,6 +24,34 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
-@app.route("/edit_category", methods=["GET", "POST"])
-def edit_category():
-    return render_template("edit_category.html")")
+
+
+# This is a Flask route that handles both GET and POST requests for editing a category.
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    # Retrieve the category with the given category_id from the database
+    # Otherwise trought a 404 error message
+    category = Category.query.get_or_404(category_id)
+
+    # Check if the incoming request method is POST(from form)
+    if request.method == "POST":  # If the form have been submitted
+        # Update the category name with the value from the form's 'category_name' input.
+        category.category_name = request.form.get("category_name")
+
+        # Commit the changes to the database.
+        db.session.commit()
+
+        # Redirect the user back to the categories page
+        return redirect(url_for("categories"))
+
+    # If the request method is GET (when the user accesses the edit category page):
+    # Render the 'edit_category.html' template and provide the category data for pre-filling the form.
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 });
+
+/* Open modal Trigger for deleting category */
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+});
+
