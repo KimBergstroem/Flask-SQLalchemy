5694ececf6e0e6bf3878ae9506b35c001822a895
KimBergstroem <>
2023-08-09 11:41:58 +0000

    Initial config database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,8 +5,8 @@ if os.path.exists("env.py"):
     import env  # noqa
 
 
-app = Flask(__name__)  # __name__ is the package name
-app.config["SECRET_KEY"]
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,14 +2,14 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category",
                             cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        # repr to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
@@ -18,12 +18,13 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
-    is_urgent = db.Column(db.Boolean, defualt=False, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    # CASCADE IS FOR DELETIN ALL TASK IN THAT CATEGORY
     category_id = db.Column(db.Integer, db.ForeignKey(
         "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        # repr to represent itself in th form of a string
-        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
